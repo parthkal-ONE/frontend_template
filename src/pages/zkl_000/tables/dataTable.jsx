132ec9b8af4fe175d5cd58df4ac6f1df625408ff
parthkal-ONE <97428134+parthkal@users.noreply.github.com>
2024-09-09 12:31:10 -0600

    Implemented Tables Instructions

--- src/pages/zkl_000/tables/dataTable.jsx
+++ src/pages/zkl_000/tables/dataTable.jsx
@@ -66,7 +66,7 @@ export default function DataGridKit(props) {
         checkboxSelection
         disableRowSelectionOnClick
         slots={{ toolbar: TableToolbar }}
-      ></DataGrid>
+      />
     </Box>
   );
 }
--- src/pages/zkl_000/tables/index.tsx
+++ src/pages/zkl_000/tables/index.tsx
@@ -3,6 +3,7 @@ import { Container, Typography } from "@mui/material";
 import DataGridKit from "./dataTable";
 import fetchNaLdfData from "@/utils/naldf-api-helper";
 import { useQuery } from "@tanstack/react-query";
+import Link from "next/link";
 
 export default function TablesPage() {
   const projectId = process.env.NEXT_PUBLIC_PROJECT_ID;
@@ -15,6 +16,149 @@ export default function TablesPage() {
   return (
     <Container>
       <h1>Tables</h1>
+      <h2>Instructions</h2>
+      <ol>
+        <li>
+          <p>
+            On pages where we require to use an MUI Data Table, we will create a
+            new file in this format:
+          </p>
+          <h5>
+            <code>{`@src/pages/<project_id>/<page_name>/index.tsx`}</code>
+          </h5>
+        </li>
+        <li>
+          <p>
+            Copy the "./dataTable.tsx" file from the example given below.
+            <br />
+            The columns for the table must be described in the{" "}
+            <code>columns</code> object:
+          </p>
+          <h5>
+            <code>{`const columns = [{`}</code>
+            <br />
+            <code style={{ paddingLeft: "1rem" }}>{`field: "header_A",`}</code>
+            <br />
+            <code
+              style={{ paddingLeft: "1rem" }}
+            >{`headerName: "Header A",`}</code>
+            <br />
+            <code
+              style={{ paddingLeft: "1rem" }}
+            >{`valueGetter: (value, row) => row?.header_A,`}</code>
+            <br />
+            <code style={{ paddingLeft: "1rem" }}>{`width: 150`}</code>
+            <br />
+            <code>{`}, ...];`}</code>
+            <br />
+          </h5>
+        </li>
+        <li>
+          <p>This is how the DataGrid Component looks:</p>
+          <h5>
+            <code>{`<DataGrid`}</code>
+            <br />
+            <code
+              style={{ paddingLeft: "1rem" }}
+            >{`loading={props.dataIsLoading}`}</code>
+            <br />
+            <code
+              style={{ paddingLeft: "1rem" }}
+            >{`getRowId={(row) => row?.<primary_key_col_name>}`}</code>
+            <br />
+            <code
+              style={{ paddingLeft: "1rem" }}
+            >{`rows={props?.inputData || []}`}</code>
+            <br />
+            <code style={{ paddingLeft: "1rem" }}>{`columns={columns}`}</code>
+            <br />
+            <code style={{ paddingLeft: "1rem" }}>{`checkboxSelection`}</code>
+            <br />
+            <code
+              style={{ paddingLeft: "1rem" }}
+            >{`disableRowSelectionOnClick`}</code>
+            <br />
+            <code
+              style={{ paddingLeft: "1rem" }}
+            >{`slots={{ toolbar: TableToolbar }}`}</code>
+            <br />
+            <code>{`/>`}</code>
+            <br />
+          </h5>
+          <p>
+            Replace <code>{`<primary_key_col_name>`}</code> with the{" "}
+            <code>field</code> property of the Primary Key Column (as defined in
+            the <code>columns</code> array.)
+          </p>
+        </li>
+        <li>
+          <p>
+            Inside{" "}
+            <code>{`@/src/pages/<project_id>/<page_name>/index.tsx`}</code>,
+            fetch the data using the NA LDF API Helper:
+            <br />
+            <h5>
+              <code>{`// EXAMPLE`}</code>
+              <br />
+              <code>{`const { data: data_tableInput, isLoading: isLoading_tableInput } = useQuery({`}</code>
+              <br />
+              <code
+                style={{ paddingLeft: "1rem" }}
+              >{`queryKey: ["table-input"],`}</code>
+              <br />
+              <code
+                style={{ paddingLeft: "1rem" }}
+              >{`queryFn: async () => await fetchNaLdfData("/zkl-000/test-table", "GET"),`}</code>
+              <br />
+              <code>{`});`}</code>
+              <br />
+            </h5>
+          </p>
+        </li>
+        <li>
+          <p>
+            Import the Table component from the <code>"./dataTable"</code> file
+            and use it here:
+          </p>
+          <h5>
+            <code>{`// EXAMPLE`}</code>
+            <br />
+            <code>{`import DataGridKit from "./dataTable";`}</code>
+            <br />
+            <code>{`...`}</code>
+            <br />
+            <code>{`export default function TablesPage() {`}</code>
+            <br />
+            <code style={{ paddingLeft: "1rem" }}>{`...`}</code>
+            <br />
+            <code style={{ paddingLeft: "1rem" }}>{`return (`}</code>
+            <br />
+            <code
+              style={{ paddingLeft: "2rem" }}
+            >{`{data_testTable && (`}</code>
+            <br />
+            <code style={{ paddingLeft: "3rem" }}>{`<DataGridKit`}</code>
+            <br />
+            <code
+              style={{ paddingLeft: "4rem" }}
+            >{`inputData={data_testTable}`}</code>
+            <br />
+            <code
+              style={{ paddingLeft: "4rem" }}
+            >{`dataIsLoading={isLoading_testTable}`}</code>
+            <br />
+            <code style={{ paddingLeft: "3rem" }}>{`/>`}</code>
+            <br />
+            <code style={{ paddingLeft: "2rem" }}>{`)}`}</code>
+            <br />
+            <code style={{ paddingLeft: "1rem" }}>{`);`}</code>
+            <br />
+            <code>{`}`}</code>
+            <br />
+          </h5>
+        </li>
+      </ol>
+      <h2>Example</h2>
       {data_testTable && (
         <DataGridKit
           inputData={data_testTable}
