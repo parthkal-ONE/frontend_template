837d3a554382a8588983ce66e0257b30b7d4b1e4
parthkal-ONE <97428134+parthkal@users.noreply.github.com>
2024-09-09 04:28:36 -0600

    Added Form Demo

--- src/pages/zkl_000/forms.tsx
+++ src/pages/zkl_000/forms.tsx
@@ -1,10 +1,34 @@
 "use client";
-import { Container } from "@mui/material";
+import { Card, Checkbox, Container, Select, TextField } from "@mui/material";
+import { useForm } from "react-hook-form";
 
 export default function FormsPage() {
+  const form = useForm({
+    defaultValues: {
+      textfield_demo: "",
+      checkbox_demo: false,
+    },
+  });
+  const { register, handleSubmit } = form;
+  const onSubmit = (data: any) => {
+    console.log(data);
+  };
+
   return (
     <Container>
       <h1>Forms</h1>
+      <form onSubmit={handleSubmit(onSubmit)} noValidate>
+        <div style={{ display: "flex", gap: "1rem" }}>
+          <TextField
+            label="Textfield Demo"
+            {...register("textfield_demo")}
+          ></TextField>
+          <Card style={{ display: "inline", padding: "1rem" }}>
+            <Checkbox {...register("checkbox_demo")} />
+            Checkbox Demo
+          </Card>
+        </div>
+      </form>
     </Container>
   );
 }
